44310f9ac73bf7644f698b2010e157515ec544de
Jinnux <nguyentandat26032001@gmail.com>
2022-06-12 17:51:32 +0700

    up

--- MVC_webapp/config/constants.php
+++ MVC_webapp/config/constants.php
@@ -3,7 +3,7 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASSWORD', '');
-define('DATABASE_NAME', 'ecommerceapp');
+define('DATABASE_NAME', 'database_212');
 
 // define('APP_ROOT', __DIR__);
 define('APP_ROOT', __DIR__.'/..');
