6504f74550ca40f77f11a6ec047fd42176a07c59
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2022-06-12 20:43:10 +0700

    Added functionality in Products page

--- mvc_webapp/src/views/screen_product/ProductScreen.php
+++ mvc_webapp/src/views/screen_product/ProductScreen.php
@@ -1,3 +1,22 @@
+<?php
+    // Prototype for a services detail, which includes the header, description and image.
+    class servicesDetail {
+        public $servicesHeader;
+        public $servicesDesc;
+        public $servicesImg;
+    }
+
+    // A list of servicesDetail given by the database
+    $servicesList = [];
+
+    // == TEST PURPOSE ==
+    // for ($i = 0; $i < 5; $i++) {
+    //     $servicesList[$i] = new servicesDetail();
+    //     $servicesList[$i]->servicesHeader = "Vietnam Community League";
+    //     $servicesList[$i]->servicesDesc = "Vietnam Community League (VCL) là hệ thống giải osu! chuyên nghiệp đầu tiên tại Việt Nam. Với lịch trình hoạt động xuyên suốt trong năm nhờ các giải đa dạng về thể thức và lực lượng hỗ trợ chuyên nghiệp, Vietnam Community League hướng tới mục tiêu đem lại trải nghiệm tốt nhất dành cho các người chơi cũng như người theo dõi.";
+    //     $servicesList[$i]->servicesImg = "./static/bg.jpg";
+    // }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -28,36 +47,15 @@
         <div id="app">
             <div id="content">
                 <div class="main-gallery js-flickity" data-flickity-options='{ "wrapAround": true }'>
-                    <div class="gallery-cell">
-                        <div class="card-title">TRONG TRƯỜNG HỢP</div>
-                        <div class="card-subText">
-                            Trong trường hợp page này bị điều tra bởi các cơ quan trực thuộc
-                            Bộ Công An (hoặc các tổ chức chính trị tương tự phục vụ cho nhà
-                            nước CHXHCNVN), tôi khẳng định page hoàn toàn không ủng hộ và
-                            giúp sức cho các hành vi chống phá Đảng và nhà nước CHXHCNVN.
-                            Tôi cũng không quên khẳng định quyết tâm chiến thắng đại dịch
-                            Covid-19, nhấn mạnh chủ quyền không thể tranh cãi với quần đảo
-                            Hoàng Sa và Trường Sa, cũng như tính chính nghĩa của cuộc chiến
-                            giải phóng người dân Campuchia khỏi thảm họa diệt chủng Khmer
-                            Đỏ.
-                        </div>
-                    </div>
-                    <div class="gallery-cell">
-                        <div class="card-title">WHEN YOU SEE IT</div>
-                        <div class="card-subText">
-                            Abbreviation for "When You See It". Used in the osu! community whenever the number 727 appears, started from a 727 pp play by Cookiezi.
-                            Aireu: Goddamnit... so close... SEVEN TWENTY SEVEN! SEVEN TWENTY SEVEN! WHEN YOU SEE IT! WHEN YOU F*CKING SEE IT! *points at the screen* SEVEN TWENTY SEVEN! When you f*cking see it.
-                            Twitch chat: *wall of text of "WYSI"*
-                        </div>
-                    </div>
-                    <div class="gallery-cell"></div>
-                    <div class="gallery-cell"></div>
-                    <div class="gallery-cell">
-                        <div class="card-title">VIETNAM COMMUNITY LEAGUE</div>
-                        <div class="card-subText">
-                            Vietnam Community League (VCL) là hệ thống giải osu! chuyên nghiệp đầu tiên tại Việt Nam. Với lịch trình hoạt động xuyên suốt trong năm nhờ các giải đa dạng về thể thức và lực lượng hỗ trợ chuyên nghiệp, Vietnam Community League hướng tới mục tiêu đem lại trải nghiệm tốt nhất dành cho các người chơi cũng như người theo dõi.
-                        </div>
-                    </div>
+                    <?php
+                        foreach ($servicesList as $idx=>$e) {
+                            echo "
+                            <div class='gallery-cell' style='background: linear-gradient(0deg, rgba(0 0 0 / 1) 0%, rgba(0 0 0 / 0) 100%), url(".$e->servicesImg.")'>
+                                <div class='card-title'>".$e->servicesHeader."</div>
+                                <div class='card-subText'>".$e->servicesDesc."</div>
+                            </div>"; 
+                        }
+                    ?>
                 </div>
             </div>
 
