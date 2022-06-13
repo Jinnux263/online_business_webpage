9be6dde5c1abdf9cd4d4ea4952a0b1bbf775f233
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2022-06-13 23:01:19 +0700

    Change from Pricing -> Services

--- mvc_webapp/src/views/includes/navbar.php
+++ mvc_webapp/src/views/includes/navbar.php
@@ -15,7 +15,7 @@
     <div class="btn" onclick="navigate('HomeController','home');">Trang chủ</div>
     <div class="btn" onclick="navigate('HomeController','about');">Thông tin</div>
     <div class="btn" onclick="navigate('ProductDetailController','products');">Bác sĩ</div>
-    <div class="btn" onclick="navigate('ProductDetailController','pricing');">Bảng giá</div>
+    <div class="btn" onclick="navigate('ProductDetailController','pricing');">Dịch vụ</div>
     <div class="btn" onclick="navigate('HomeController','contact');">Liên hệ</div>
     <div class="btn" onclick="navigate('PostsController','news');">Tin tức</div>
     <?php
