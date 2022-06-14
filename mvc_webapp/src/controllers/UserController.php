47e1ed6b5f7b69edaa122abfbc383830364d0c73
Jinnux <nguyentandat26032001@gmail.com>
2022-06-14 07:24:12 +0700

    update

--- mvc_webapp/src/controllers/UserController.php
+++ mvc_webapp/src/controllers/UserController.php
@@ -2,6 +2,9 @@
 require_once dirname(__DIR__) . '/controllers/base_controller/BaseController.php';
 require_once APP_ROOT . '/src/controllers/AuthenticationController.php';
 require_once APP_ROOT . '/src/models/User.php';
+require_once APP_ROOT . '/src/models/Bill.php';
+require_once APP_ROOT . '/src/models/Service.php';
+require_once APP_ROOT . '/src/models/News.php';
 
 
 class UserController extends BaseController
@@ -49,11 +52,8 @@ class UserController extends BaseController
     $isAdmin = $_SESSION['isAdmin'];
 
     // List of bills taken from database
-    $billAdminList = [];
-
-    // == TEST PURPOSE ==
-    for ($i = 0; $i < 5; $i++)
-      $billAdminList[$i] = new billAdminDetail("Dịch vụ tư vấn tâm lý trực tiếp", "#009849", "12/2/2022", "Lê Van Dovious Panda", "dvs_pd@ppy.sh", "0727 727 727");
+    $billModel = New BillModel();
+    $billAdminList = $billModel->getAll();
 
     $data = array('isAdmin' => $isAdmin, 'billAdminList' => $billAdminList);
     $this->render('BillsAdminScreen', $data);
@@ -67,24 +67,8 @@ class UserController extends BaseController
     $isAdmin = $_SESSION['isAdmin'];
 
     // A list of productsDetail given by the database
-    $servicesList = [];
-
-    // == TEST PURPOSE ==
-    $servicesList[0] = new servicesDetail(
-      "plan-1",
-      "Dịch vụ tư vấn trực tiếp tại trung tâm",
-      "60% khách hàng là bệnh nhân của chúng tôi đã quyết định đến trung tâm để tham gia vào các khóa điều trị cộng đồng. Kết quả mang lại là rất rõ rệt và được đánh giá rất cao trong số các khách hàng. Nếu bạn cảm thấy không phiền, hãy đến với trung tâm của chúng tôi để nhận được nhiều ưu đãi hơn.",
-      "1.000.000VNĐ / 8 tuần điều trị",
-      "https://bookingcare.vn/files/blog/2020/12/30/100527-tu-van-tam-ly.jpg"
-    );
-
-    $servicesList[1] = new servicesDetail(
-      "plan2",
-      "Dịch vụ tư vấn trực tuyến",
-      "Trong mùa dịch bệnh đang hoành hành, nhiều khách hàng cảm thấy rất e ngại với việc tiếp xúc gần cũng như phải ra ngoài. Thấu hiểu được điều đó, mô hình chăm sóc và tư vấn sức khỏe tâm lý của khách hàng trực tuyến đã được chúng tôi triển khai và tiến hành từ rất sớm. Các bạn sẽ được cung cấp một hệ thống để liên lạc với các bác sĩ 24/7.",
-      "2.000.000VNĐ / 16 tuần điều trị",
-      "https://cdn.bookingcare.vn/fr/w800/2020/12/21/165610-tu-van-tam-ly-online.jpg"
-    );
+    $serviceModel = New ServiceModel();
+    $servicesList = $serviceModel->getAll();
 
     $data = array('isAdmin' => $isAdmin, 'servicesList' => $servicesList);
     $this->render('ServicesAdminScreen', $data);
@@ -98,27 +82,8 @@ class UserController extends BaseController
     // Variable for account type
     $isAdmin = $_SESSION['isAdmin'];
 
-
-    // A list of news taken from database
-    $newsList = [];
-
-    // == TEST PURPOSE ==
-    $titleList = [
-      "Điều trị tâm lý cho trẻ vị thành niên - Bài toàn mới trong ngành tâm lý?",
-      "Hikikomori là gì và tác động của nó đến giới trẻ Nhật Bản",
-      "Trầm cảm và những biểu hiện ban đầu",
-      "Trầm cảm và những biểu hiện ban đầu"
-    ];
-
-    $imgList = [
-      "https://ounews.co/wp-content/uploads/2018/03/shutterstock_174741554-e1519911636917.jpg",
-      "https://kenh14cdn.com/thumb_w/650/2016/hikiko-1471883423991.jpg",
-      "https://vinmec-prod.s3.amazonaws.com/images/20210320_072716_425735_tram-cam.max-1800x1800.jpg",
-      "https://vinmec-prod.s3.amazonaws.com/images/20210320_072716_425735_tram-cam.max-1800x1800.jpg"
-    ];
-
-    for ($i = 0; $i < 4; $i++)
-      $newsList[$i] = new newsPreview($titleList[$i], $imgList[$i]);
+    $newsModel = New ServiceModel();
+    $newsList = $newsModel->getAll();
 
     $data = array('isAdmin' => $isAdmin, 'newsList' => $newsList);
     $this->render('PostAdminScreen', $data);
